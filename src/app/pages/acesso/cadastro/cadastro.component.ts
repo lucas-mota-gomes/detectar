2427708c1621e68395a4560fee1f70fff0f6a29f
Lucas Mota <lcasmotagomes@hotmail.com>
2024-09-27 20:21:30 -0300

    Refactor image URLs to use environment variable

--- src/app/pages/acesso/cadastro/cadastro.component.ts
+++ src/app/pages/acesso/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { MessageService } from 'primeng/api';
@@ -35,7 +35,7 @@ export class CadastroComponent implements OnInit {
     RG: [null, [Validators.required]],
     address: [null, [Validators.required]],
     celular: [null, [Validators.required]],
-    email: [null, [Validators.required, Validators.email]],
+    email: [null, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
     password: [null, [Validators.required, Validators.minLength(8)]],
   });
 
@@ -45,7 +45,7 @@ export class CadastroComponent implements OnInit {
     RG: [null, [Validators.required]],
     address: [null, [Validators.required]],
     celular: [null, [Validators.required]],
-    email: [null, [Validators.required, Validators.email]],
+    email: [null, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
     password: [null, [Validators.required, Validators.minLength(8)]],
     speciality: [null, [Validators.required]],
     description: [null, [Validators.required]]
@@ -118,6 +118,93 @@ export class CadastroComponent implements OnInit {
         this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao realizar cadastro!' });
       });
     }
+  }
+
+  public maskCpfCnpj(form: AbstractControl | null) {
+    if(form === null) return;
+    let value = form.value;
+    if(value.length > 18) {
+      form.setValue(value.substring(0, 18));
+      return;
+    }
+    if (value.length <= 14) {
+      form.setValue(value.replace(/\D/g, '').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, '$1-$2'));
+      if(value.length === 14 && !this.isValidCPF(value)) {
+        form.setErrors({invalid: true});
+      }
+    } else {
+      form.setValue(value.replace(/\D/g, '').replace(/(\d{2})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1/$2').replace(/(\d{4})(\d{1,2})/, '$1-$2'));
+      if(value.length === 18 && !this.isValidCNPJ(value)) {
+        form.setErrors({invalid: true});
+      }
+    }
+  }
+
+  public isValidCPF(cpf: string): boolean {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+      return false;
+    }
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+      sum += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let remainder = 11 - (sum % 11);
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+    if (remainder !== parseInt(cpf.charAt(9))) {
+      return false;
+    }
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+      sum += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    remainder = 11 - (sum % 11);
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+    return remainder === parseInt(cpf.charAt(10));
+  }
+
+  public isValidCNPJ(cnpj: string): boolean {
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+    if (cnpj.length !== 14 || /^(\d)\1+$/.test(cnpj)) {
+      return false;
+    }
+
+    let length = cnpj.length - 2;
+    let numbers = cnpj.substring(0, length);
+    let digits = cnpj.substring(length);
+    let sum = 0;
+    let pos = length - 7;
+
+    for (let i = length; i >= 1; i--) {
+      sum += parseInt(numbers.charAt(length - i)) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    let result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+    if (result !== parseInt(digits.charAt(0))) {
+      return false;
+    }
+
+    length = length + 1;
+    numbers = cnpj.substring(0, length);
+    sum = 0;
+    pos = length - 7;
+
+    for (let i = length; i >= 1; i--) {
+      sum += parseInt(numbers.charAt(length - i)) * pos--;
+      if (pos < 2) {
+        pos = 9;
+      }
+    }
+
+    result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+    return result === parseInt(digits.charAt(1));
   }
 
 
